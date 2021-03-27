9888d902ae86108698bc95eaa8ae6c1a5e086d7a
George221b <george221b@gmail.com>
2021-03-27 17:04:20 +0200

    Microsoft SignalR added

---
+++ Screenshare/Source/Screenshare.Main/Hubs/ScreencastingHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Screenshare.Main.Hubs
+{
+	public class ScreencastingHub : Hub
+	{
+		public void Hello()
+		{
+			Clients.All.hello();
+		}
+	}
+}
